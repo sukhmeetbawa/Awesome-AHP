0f3321deb347e2722bb187a282b56773b5adf4a2
Sukhmeet <86137661+sukhmeetbawa@users.noreply.github.com>
2023-10-06 03:01:21 +0530

    Add error toast notifiaction component

---
+++ frontend/src/components/ErrorToast.tsx
@@ -0,0 +1,37 @@
+// ErrorToast.tsx
+import React from "react";
+import Toast from "react-bootstrap/Toast";
+import ToastContainer from "react-bootstrap/ToastContainer";
+
+interface ErrorToastProps {
+    show: boolean;
+    onClose: () => void;
+    error: string | null;
+}
+
+const ErrorToast: React.FC<ErrorToastProps> = ({ show, onClose, error }) => {
+    return (
+        <ToastContainer position="top-end" className="p-3">
+            <Toast
+                onClose={onClose}
+                show={show}
+                delay={5000}
+                autohide
+                bg="warning"
+            >
+                <Toast.Header>
+                    <strong className="me-auto">Error</strong>
+                </Toast.Header>
+                {error ? (
+                    <Toast.Body>{error}</Toast.Body>
+                ) : (
+                    <Toast.Body>
+                        Something went wrong. Please try again.
+                    </Toast.Body>
+                )}
+            </Toast>
+        </ToastContainer>
+    );
+};
+
+export default ErrorToast;
