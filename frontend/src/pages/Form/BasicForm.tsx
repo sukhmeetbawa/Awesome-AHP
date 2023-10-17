3c841eb13ee8d4d657bb834f02165fcc2c09b6e5
Sukhmeet <86137661+sukhmeetbawa@users.noreply.github.com>
2023-10-17 08:04:32 +0530

    Add ui for Basic Form

--- frontend/src/pages/Form/BasicForm.tsx
+++ frontend/src/pages/Form/BasicForm.tsx
@@ -1,24 +1,38 @@
 import NextIcon from "@mui/icons-material/SkipNextRounded";
 import BackIcon from "@mui/icons-material/SkipPreviousRounded";
-import { Button, Typography } from "@mui/material";
+import { Button, Stack, TextField, Typography } from "@mui/material";
 interface BasicFormProps {
     nextStep: () => void;
 }
 const BasicForm: React.FC<BasicFormProps> = ({ nextStep }) => {
     return (
-        <>
+        <Stack spacing={2} margin={2}>
             <Typography variant="h1">Basic Details</Typography>
-            <Button variant="contained" startIcon={<BackIcon />} disabled>
-                Back
-            </Button>
-            <Button
-                onClick={nextStep}
-                variant="contained"
-                endIcon={<NextIcon />}
-            >
-                Next
-            </Button>
-        </>
+            <div>
+                <Typography variant="h2">Criteria</Typography>
+                <TextField size="small" label="Enter the Criterias" />
+            </div>
+            <div>
+                <Typography variant="h2">Alternative</Typography>
+                <TextField size="small" label="Enter the Alternative" />
+            </div>
+            <div>
+                <Typography variant="h2">Usecase</Typography>
+                <TextField size="small" label="Enter the Usecase" />
+            </div>
+            <div>
+                <Button variant="contained" startIcon={<BackIcon />} disabled>
+                    Back
+                </Button>
+                <Button
+                    onClick={nextStep}
+                    variant="contained"
+                    endIcon={<NextIcon />}
+                >
+                    Next
+                </Button>
+            </div>
+        </Stack>
     );
 };
 
