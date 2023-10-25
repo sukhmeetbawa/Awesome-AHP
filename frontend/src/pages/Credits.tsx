29c97361c772642a09c816a22f93e4a450dab8c6
Sukhmeet <86137661+sukhmeetbawa@users.noreply.github.com>
2023-10-25 15:09:48 +0530

    Update credits page

--- frontend/src/pages/Credits.tsx
+++ frontend/src/pages/Credits.tsx
@@ -1,19 +1,27 @@
+import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import { Grid, Link, Typography } from "@mui/material";
 
 const teamMembers = [
     {
         name: "Sukhmeet Singh Bawa",
-        link: "https://www.linkedin.com/in/sukhmeet-bawa/",
+        linkedin: "https://www.linkedin.com/in/sukhmeet-bawa/",
+        github: "https://github.com/sukhmeetbawa",
     },
     {
         name: "Devarsh Ukani",
-        link: "https://www.linkedin.com/in/devarshukani/",
+        linkedin: "https://www.linkedin.com/in/devarshukani/",
+        github: "https://github.com/devarshukani",
     },
-    { name: "Sneh Patel", link: "https://www.linkedin.com/in/ssnehppatel/" },
+    {
+        name: "Sneh Patel",
+        linkedin: "https://www.linkedin.com/in/ssnehppatel/",
+        github: "https://github.com/Patel-Sneh",
+    },
     {
         name: "Vaishnavi Diwan",
-        link: "https://www.linkedin.com/in/vaishnavi-diwan-231850267/",
+        linkedin: "https://www.linkedin.com/in/vaishnavi-diwan-231850267/",
+        github: "https://github.com/vaishnavidiwan",
     },
 ];
 
@@ -21,27 +29,30 @@ const Credits = () => {
     return (
         <Grid container>
             {teamMembers.map((member, index) => (
-                <Grid key={index} container item xs={12} sm={6}>
-                    <Grid item xs>
-                        <Typography variant="h6">
-                            <Link
-                                href={member.link}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                color="inherit"
-                                underline="none"
-                                display="flex"
-                                alignItems="center"
-                            >
-                                {member.name}
-                                <LinkedInIcon
-                                    sx={{
-                                        margin: 1,
-                                    }}
-                                />
-                            </Link>
-                        </Typography>
-                    </Grid>
+                <Grid key={index} item xs={12} sm={6}>
+                    <Typography variant="h6" display="flex" alignItems="center">
+                        {member.name}
+                        <Link
+                            href={member.linkedin}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            color="inherit"
+                            underline="none"
+                        >
+                            <LinkedInIcon
+                                sx={{ marginLeft: 1, marginRight: 1 }}
+                            />
+                        </Link>
+                        <Link
+                            href={member.github}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            color="inherit"
+                            underline="none"
+                        >
+                            <GitHubIcon sx={{ marginLeft: 1 }} />
+                        </Link>
+                    </Typography>
                 </Grid>
             ))}
         </Grid>
